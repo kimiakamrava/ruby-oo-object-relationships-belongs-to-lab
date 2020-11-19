30a83a523aea895766132ab1aca675a92033ac31
kimiakamrava <kimiakamrava93@gmail.com>
2020-11-18 16:24:31 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist
+    attr_accessor :name
+    def initialize()
+        @name = nil
+    end
+end
+irvin = Artist.new()
+  
---
+++ run.rb
@@ -0,0 +1,7 @@
+
+
+require_relative 'lib/artist.rb'
+require_relative 'lib/author.rb'
+
+require_relative 'models/post.rb'
+require_relative 'models/song.rb'
